89f463381505f2555a5fe6b629a839e33dbbf780
afri-bit <20866800+afri-bit@users.noreply.github.com>
2024-01-21 00:21:03 +0100

    Added conan API manager class
    Conan API manager is meant to manage the switching between conan version
    and its API. This class will apply factory pattern to generate the API
    instance according to given conan version

---
+++ src/conans/api/conanAPIManager.ts
@@ -0,0 +1,52 @@
+import { Conan1API } from "../conan/api/conanAPI";
+import { Conan2API } from "../conan2/api/conanAPI";
+import { ConanAPI, ConanExecutionMode } from "./base/conanAPI";
+
+export class ConanAPIManager {
+    private _conanVersion: string = "1";
+    private _conanApi: ConanAPI | undefined = undefined;
+
+    public constructor(conanVersion: string, pythonInterpreter: string, conanExecutable: string, conanExecutionMode: ConanExecutionMode) {
+        this.setApiInstance(conanVersion, pythonInterpreter, conanExecutable, conanExecutionMode);
+    }
+
+    public setApiInstance(conanVersion: string, pythonInterpreter: string, conanExecutable: string, conanExecutionMode: ConanExecutionMode) {
+        if (conanVersion == "1") {
+            this._conanApi = new Conan1API(pythonInterpreter, conanExecutable, conanExecutionMode);
+        }
+        else if (conanVersion == "2") {
+            this._conanApi = new Conan2API(pythonInterpreter, conanExecutable, conanExecutionMode);
+        }
+        else {
+            this._conanApi = undefined;
+        }
+    }
+
+    public get conanVersion(): string {
+        return this._conanVersion
+    }
+
+    public get conanApi(): ConanAPI | undefined {
+        return this._conanApi;
+    }
+
+    public switchExecutionMode(mode: ConanExecutionMode): void {
+        this.conanApi?.switchExecutionMode(mode);
+    }
+
+    public setPythonInterpreter(pythonInterpreter: string): void {
+        this.conanApi?.setPythonInterpreter(pythonInterpreter);
+    }
+
+    public switchToPythonMode(pythonInterpreter: string): void {
+        this.conanApi?.switchToPythonMode(pythonInterpreter);
+    }
+
+    public setConanExecutable(conanExecutable: string): void {
+        this.conanApi?.setConanExecutable(conanExecutable);
+    }
+
+    public switchToConanExecutableMode(conanExecutable: string): void {
+        this.conanApi?.switchToConanExecutableMode(conanExecutable);
+    }
+}
