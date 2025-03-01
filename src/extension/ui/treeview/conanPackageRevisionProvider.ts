f8b1a8b69cd9909a71e783cb7f1f8adbb17120de
afri-bit <afri.bit@outlook.com>
2025-03-02 00:00:10 +0100

    Added code documentation from the last changes

--- src/extension/ui/treeview/conanPackageRevisionProvider.ts
+++ src/extension/ui/treeview/conanPackageRevisionProvider.ts
@@ -23,6 +23,13 @@ export class ConanPackageRevisionNodeProvider implements vscode.TreeDataProvider
         this.settingsPropertyManager = settingsPropertyManager;
     }
 
+    /**
+     * Refreshes the tree view with the provided recipe name, package ID, and dirty package flag.
+     * 
+     * @param recipeName - The name of the recipe to refresh.
+     * @param packageId - The ID of the package to refresh.
+     * @param showDirtyPackage - Flag to indicate whether to show dirty packages.
+     */
     public refresh(recipeName: string, packageId: string, showDirtyPackage: boolean): void {
         this.recipeName = recipeName;
         this.packageId = packageId;
@@ -39,12 +46,13 @@ export class ConanPackageRevisionNodeProvider implements vscode.TreeDataProvider
         if (!element) {
 
             let packageRevisionList: Array<ConanPackageRevision> = [];
+            // TODO: Implement dirty package list
             let dirtyPackageList: Array<ConanPackageRevision> = [];
             let packageRevisionItemList: Array<ConanPackageRevisionItem> = [];
-    
+
             if (this.conanApiManager.conanApi) {
                 packageRevisionList = this.conanApiManager.conanApi.getPackageRevisions(this.recipeName, this.packageId);
-    
+
                 for (let pkgRevision of packageRevisionList) {
                     let packageRevisionPath = this.conanApiManager.conanApi.getPackageRevisionPath(this.recipeName, this.packageId, pkgRevision.id);
 
@@ -63,16 +71,27 @@ export class ConanPackageRevisionNodeProvider implements vscode.TreeDataProvider
         if (!this.expandedNodes.has(element.resourceUri.fsPath)) {
             return [];
         }
-        
+
         return this.getFilesAndFolders(element.resourceUri.fsPath);
 
     }
 
+    /**
+     * Method to get files and folders from a directory of the package revision path.
+     * It reads the directory contents, creates `ConanPackageRevisionItem` for each file/folder,
+     * and sorts them with folders first.
+     * 
+     * @param directoryPath - The path of the directory to read.
+     * @returns An array of `ConanPackageRevisionItem` representing the files and folders.
+     */
     private getFilesAndFolders(directoryPath: string): ConanPackageRevisionItem[] {
+
+        // Check if the directory exists
         if (!fs.existsSync(directoryPath)) {
             return [];
         }
 
+        // Read the directory contents and return the tree items
         const files = fs.readdirSync(directoryPath).map(file => {
             const filePath = path.join(directoryPath, file);
             const stat = fs.statSync(filePath);
@@ -81,7 +100,7 @@ export class ConanPackageRevisionNodeProvider implements vscode.TreeDataProvider
                 file,
                 vscode.Uri.file(filePath),
                 stat.isDirectory() ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
-                new ConanPackageRevision("",123),
+                new ConanPackageRevision("", 123),
                 false,
                 stat.isDirectory()
             );
@@ -115,10 +134,11 @@ export class ConanPackageRevisionItem extends vscode.TreeItem {
 
     constructor(public readonly label: string, public readonly resourceUri: vscode.Uri, public readonly collapsibleState: vscode.TreeItemCollapsibleState, model: ConanPackageRevision, isRoot: boolean, isDirectory: boolean = false) {
 
-        super(resourceUri=resourceUri, collapsibleState=collapsibleState);
+        super(resourceUri = resourceUri, collapsibleState = collapsibleState);
 
         this.label = label;
 
+        // Internal state to determine if this item is a folder
         this.isDirectory = isDirectory;
 
         this.command = {
@@ -127,17 +147,27 @@ export class ConanPackageRevisionItem extends vscode.TreeItem {
         };
 
         if (isRoot) {
+            // Conan package revision is now expandable to be able to see the content of the package revision
+            // This check is necessary to check if the item is the root of the package revision treeview item
+
+
+            // In case fo the root item, the icon is set to the package icon
             this.iconPath = {
                 light: vscode.Uri.file(path.join(__filename, '..', '..', '..', '..', '..', '..', 'resources', 'icon', 'package_revision.png')),
                 dark: vscode.Uri.file(path.join(__filename, '..', '..', '..', '..', '..', '..', 'resources', 'icon', 'package_revision.png'))
             };
 
+            // Set the context to show all the buttons that are pre-configured by the `package.json` file.
             this.contextValue = 'packageRevision';
+
+
+            // Feed model with package revision data
             this.model = model;
 
+            // Set the tooltip to show the package revision data in JSON format
             this.tooltip = JSON.stringify(this.model, null, 4);
         }
 
-        
+
     }
 }
